d8db5777756e6a57442a34674e046db03a1e818d
Italo Menezes <italomlporoca@hotmail.com>
2018-05-17 14:38:49 -0300

    ESLint rules in accordance with PMD

--- app/models/user.js
+++ app/models/user.js
@@ -4,24 +4,24 @@ const UserSchema = mongoose.Schema({
   created_at: {
     type: Date,
     required: true,
-    default: Date.now,
+    default: Date.now
   },
   username: {
     type: String,
     trim: true,
     unique: true,
-    required: true,
+    required: true
   },
   password: {
     type: String,
     trim: true,
-    required: true,
+    required: true
   },
   name: {
     type: String,
     trim: true,
-    required: true,
-  },
+    required: true
+  }
 });
 
 module.exports = () => mongoose.model('User', UserSchema);
