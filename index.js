ddcc6b6f31f8af4d59e222115494dfc0ec483954
Italo Menezes <italomlporoca@hotmail.com>
2018-05-23 21:08:26 -0300

    Resetting firebase with properly project

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const http = require('http');
+require('./config/firebase')();
 const app = require('./config/express')();
 require('./config/database')('mongodb://localhost/esbackend');
 
 const port = app.get('port');
 
-http.createServer(app)
-  .listen(port, () => {
-    console.log(`Express Server listening on port ${port}`);
-  });
+http.createServer(app).listen(port, () => {
+  console.log(`Express Server listening on port ${port}`);
+});
