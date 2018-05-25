9918372b7d7c48898a85b1faa49f121574012f02
Jhonatan <jhonatanbds@outlook.com>
2018-05-25 12:45:51 -0300

    CRUD operations added to car controller

--- app/controllers/car.js
+++ app/controllers/car.js
@@ -1,32 +1,32 @@
-module.exports = () => {
-  // const Car = app.models.car;
+module.exports = (app) => {
+  const Car = app.models.car;
 
   const controller = {};
 
   // Display all cars on GET.
-  controller.list = (req, res) => {
-    res.send('NOT IMPLEMENTED: car list');
-  };
+  controller.list = () =>
+    Car.find();
 
   // Display detail page for a specific car on GET.
-  controller.detail = (req, res) => {
-    res.send(`NOT IMPLEMENTED: car detail: ${req.params.id}`);
-  };
+  controller.detail = (id) =>
+    Car.findById(id).exec();
 
   // Handle car create on POST.
-  controller.create = (req, res) => {
-    res.send('NOT IMPLEMENTED: car create POST');
+  controller.create = (brand, model, year, plate) => {
+    const car = new Car({
+      brand, model, year, plate
+    });
+    return car.save();
   };
 
   // Handle car delete on DELETE.
-  controller.delete = (req, res) => {
-    res.send('NOT IMPLEMENTED: car delete DELETE');
-  };
+  controller.delete = (id) =>
+    Car.findByIdAndRemove(id);
 
   // Display car update form on PUT.
-  controller.update = (req, res) => {
-    res.send('NOT IMPLEMENTED: car update PUT');
-  };
+  controller.update = (id, brand) =>
+    Car.findByIdAndUpdate(id, { $set: { brand } });
+
 
   return controller;
 };
